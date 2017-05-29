cb9d5e011e22944703c08be3a04666c02a065e67
Kendel Chopp <conact@kchopp.com>
2017-05-29 09:48:12 -0500

    Initial Commit

---
+++ LineupGenerator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LineupGenerator
+//
+//  Created by Kendel Chopp on 5/29/17.
+//  Copyright © 2017 Kendel Chopp. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
